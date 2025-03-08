5aa538a5b25432b17ec40ef26b9d8167271ad697
GR4V1TY0123 <mandarekal2@gmail.com>
2025-03-08 14:53:25 +0530

    ok

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask,render_template
 
 app=Flask(__name__)
@@ -12,12 +11,3 @@ if __name__=='__main_':
 zcxzcxz
 
    # hiihihihifdfsfdf
-=======
-etgfgdfgsadasd
-hiii
-<<<<<<< HEAD
->>>>>>> 03a42912a1582f5c2f5bda4082133b9499b5e7d9
-=======
-
-mandar
->>>>>>> b4371d170806fc41ba5c7649b558a40ca05e8072
